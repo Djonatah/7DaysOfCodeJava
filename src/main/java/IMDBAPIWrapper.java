8268a7af62ca349251cb473185bf06459acc2f6f
Djonatah <djonatah@gmail.com>
2023-04-28 20:57:22 -0300

    #Day1: Retrive top 250 rated movies from IMDB and print JSON response to console

--- src/main/java/IMDBAPIWrapper.java
+++ src/main/java/IMDBAPIWrapper.java
@@ -10,31 +10,26 @@ import java.net.http.HttpResponse;
  *
  * #7DaysOfCode challenge
  */
-public class IMDBWrapper {
+public class IMDBAPIWrapper {
     private static final String API_KEY = "k_9g3b3duz";
     private static final String LANGUAGE = "en";
     private static final String API_BASE = "https://imdb-api.com/" + LANGUAGE + "/API";
 
+    private HttpClient httpClient = null;
+
     /**
-     * Retrieve the top 250 rated movies from IMDB
-     * @return JSON containing all 250 top rated movies
+     * Retrieves the top 250 rated movies from IMDB
+     *
+     * @return JSON containing all top 250 rated movies from IMDB
      * @throws URISyntaxException
      * @throws IOException
      * @throws InterruptedException
      */
     public String getTop250Movies() throws URISyntaxException, IOException, InterruptedException {
 
-        HttpClient client = HttpClient
-                .newBuilder()
-                .build();
-
-        String uri = getURI("Top250Movies");
-
-        HttpRequest request = HttpRequest
-                .newBuilder()
-                .GET()
-                .uri(new URI(uri))
-                .build();
+        String requestURI = getURI("Top250Movies");
+        HttpClient client = getHttpClient();
+        HttpRequest request = buildGetRequest(requestURI);
 
         HttpResponse.BodyHandler bodyHandler = HttpResponse.BodyHandlers.ofString();
 
@@ -42,6 +37,33 @@ public class IMDBWrapper {
         String json = response.body().toString();
 
         return json;
+    }
+
+    /**
+     * Build GET request based on URI
+     * @param uri
+     * @return
+     * @throws URISyntaxException
+     */
+    private static HttpRequest buildGetRequest(String uri) throws URISyntaxException {
+        return HttpRequest
+                .newBuilder()
+                .GET()
+                .uri(new URI(uri))
+                .build();
+    }
+
+    /**
+     * Get the HttpClient for this wrapper
+     * @return
+     */
+    private HttpClient getHttpClient() {
+        if(httpClient == null) {
+            httpClient = HttpClient
+                    .newBuilder()
+                    .build();
+        }
+        return httpClient;
     }
 
     /**
@@ -53,8 +75,16 @@ public class IMDBWrapper {
         String endpoint = API_BASE + "/" + method + "/" + API_KEY;
         return endpoint;
     }
+
+    /**
+     * Main executing class
+     * @param args
+     * @throws URISyntaxException
+     * @throws IOException
+     * @throws InterruptedException
+     */
     public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
-        IMDBWrapper wrapper = new IMDBWrapper();
+        IMDBAPIWrapper wrapper = new IMDBAPIWrapper();
         String json = wrapper.getTop250Movies();
         System.out.println(json);
     }
